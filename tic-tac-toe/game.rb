21bc994282776e14fcc04862f2ad7f258a4a6c8b
Anand-1812 <anand.work2005@gmail.com>
2025-02-13 19:50:46 +0530

    change the player name

--- tic-tac-toe/game.rb
+++ tic-tac-toe/game.rb
@@ -4,8 +4,8 @@ require_relative "board"
 class Game
   def initialize
     @board = Board.new
-    @player1 = Player.new("Player 1", "X")
-    @player2 = Player.new("Player 2", "O")
+    @player1 = Player.new("Anand", "X")
+    @player2 = Player.new("Natsu", "O")
     @current_player = @player1
   end
 
